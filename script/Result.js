d9804d1dce57c4f8944cc1a71f317103e9bb71f8
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-12 18:39:46 +0200

    class result - checks if the person has won and if so returns the win

---
+++ script/Result.js
@@ -0,0 +1,15 @@
+class Result {
+
+    static moneyWinInGame(result, bid) {
+        if (result) {
+            return 3 * bid;
+        } else {
+            return 0;
+        }
+    }
+
+    static checkWinner(draw){
+        return draw[0] === draw[1] && draw[1] === draw[2];
+    }
+
+}
